968dfb4ba9e93e59dee7273e434d0f290776b531
Benji Altman <benji@altmansoftwaredesign.com>
2021-11-04 20:18:56 -0500

    adding asyncBinary reader

---
+++ Gerk.BinaryExtension/AsyncBinaryReader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime;
+using System.Text;
+using System.Globalization;
+using System.Diagnostics.Contracts;
+using System.Security;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Gerk.BinaryExtension
+{
+	public class AsyncBinaryReader : IDisposable
+	{
+		public Stream BaseStream { get; private set; }
+		private bool leaveOpen;
+
+		public AsyncBinaryReader(Stream stream, bool leaveOpen = false)
+		{
+			BaseStream = stream;
+			this.leaveOpen = leaveOpen;
+		}
+
+		public void Dispose()
+		{
+			if (!leaveOpen)
+				((IDisposable)BaseStream).Dispose();
+		}
+
+		public async Task<int> Read()
+	}
+}
